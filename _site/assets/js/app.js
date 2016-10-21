544715b30de03412d0ceefa0dc9aa4b354a1ce1c
Dennis Hatwieger <hej@dennishatwieger.de>
2016-10-21 16:16:49 +0200

    v01

---
+++ _site/assets/js/app.js
@@ -0,0 +1,153 @@
+/*var Tabs = (function() {
+  var s;
+
+  return {
+    settings: {
+      tabs: document.getElementsByClassName('tabs__item'),
+      tab: document.getElementsByClassName('tab')
+    },
+
+    init: function() {
+      s = this.settings;
+      this.display();
+      this.click();
+    },
+
+    display: function() {
+      if (s.tab.length) {
+        [].forEach.call(s.tab, function(tab) {
+          tab.style.display = 'none';
+        });
+        s.tab[0].style.display = 'block';
+        s.tab[0].classList.add('active');
+        s.tabs[0].classList.add('active');
+      }
+    },
+
+    click: function() {
+      if (s.tabs.length) {
+        var currentIdx = 0,
+            prevIdx = currentIdx;
+
+        [].forEach.call(s.tabs, function(tab, idx) {
+          tab.addEventListener('click', function() {
+            prevIdx = currentIdx;
+            currentIdx = idx;
+
+            if (prevIdx !== currentIdx) {
+              s.tab[prevIdx].style.display = 'none';
+              s.tab[prevIdx].classList.remove('active');
+              s.tabs[prevIdx].classList.remove('active');
+              s.tab[currentIdx].style.display = 'block';
+              s.tab[currentIdx].classList.add('active');
+              s.tabs[currentIdx].classList.add('active');
+            }
+          });
+        });
+      }
+    }
+
+  }
+})();
+
+var Preview = (function() {
+  var s;
+
+  return {
+    settings: {
+      img: document.getElementsByClassName('preview__img'),
+      post: document.getElementsByClassName('preview')
+    },
+
+    init: function() {
+      s = this.settings;
+      this.display();
+      this.mouseenter();
+    },
+
+    display: function() {
+      if (s.img.length) {
+        [].forEach.call(s.img, function(img) {
+          img.style.display = 'none';
+        });
+        s.img[0].style.display = 'block';
+      }
+    },
+
+    mouseenter: function() {
+      if (s.post.length) {
+        var currentIdx = 0,
+            prevIdx = currentIdx;
+
+        [].forEach.call(s.post, function(preview, idx) {
+          preview.addEventListener('mouseenter', function() {
+            prevIdx = currentIdx;
+            currentIdx = idx;
+
+            if (prevIdx !== currentIdx) {
+              s.img[prevIdx].style.display = 'none';
+              s.img[currentIdx].style.display = 'block';
+            }
+          });
+        });
+      }
+    }
+  }
+})();
+
+var wow = new WOW({
+  animateClass: 'fade-in'
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  Tabs.init();
+  Preview.init();
+  wow.init();
+});*/
+ // cache the navigation links 
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        
+        $('a').each(function () {
+           // $(this).css('color', '#2A2E32');
+        })
+        $(this).css('color', '' + $(this).attr('id'));
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    var windowHeight = $(window).height();
+    var windowHeight_half = (windowHeight / 2);
+    $('#menu-center a').each(function () {
+
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            currLink.css('color', '' + $(this).attr('id'));
+            
+        }
+        else{
+            currLink.css('color', '#2A2E32');
+        }
+
+        
+    });
+}
